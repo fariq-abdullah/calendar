ce2bcbd621c90fe86119ca83a6cfd52fe2c4731a
Fariq Abdullah <fariqabdullah41@gmail.com>
2020-10-20 12:53:16 +0600

    added api calls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-import React from 'react';
-import MyCalendar from './MyCalendar';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { ApiProvider, useGet } from 'react-rest-api'
+
+const Dates = () => {
+  const { loading, result, error, } = useGet('gToH?date=20-10-2020')
+
+  if (loading) {
+    return 'loading ...'
+  }
+
+  if (error) {
+    return error.toString()
+  }
+
+  return JSON.stringify(result, null, 2)
+}
+
+const resolveCallback = response => response.json()
 
 function App() {
 
@@ -7,9 +25,27 @@ function App() {
     textAlign: "-webkit-center"
   };
 
+  const [value, onChange] = useState(new Date());
+
+  const [apiConfig, setApiConfig] = useState({
+    url: 'http://api.aladhan.com/v1/',
+    config: {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    },
+    resolveCallback
+  })
+
   return (
     <div style={mystyle}>
-      <MyCalendar />
+      <Calendar
+        onChange={onChange}
+        value={value}
+      />
+      <ApiProvider {...apiConfig} setConfig={setApiConfig}>
+        <Dates />
+      </ApiProvider>
     </div>
     
   );
