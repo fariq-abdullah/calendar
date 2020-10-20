cd3c7961b0521dbf28ab251631ec99f8b389d757
Fariq Abdullah <fariqabdullah41@gmail.com>
2020-10-20 08:11:57 +0600

    Updates

---
+++ precache-manifest.2ee796bf5db5243fcb6fdee98b43642e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a295918a8ee6fafe4d417673b7da7b20",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "1e5e9093a8bd685cc9b7",
+    "url": "/calendar/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "729618db8e849b6ec285",
+    "url": "/calendar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e5e9093a8bd685cc9b7",
+    "url": "/calendar/static/js/2.3cbd2372.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/calendar/static/js/2.3cbd2372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729618db8e849b6ec285",
+    "url": "/calendar/static/js/main.650fb2f1.chunk.js"
+  },
+  {
+    "revision": "7d0fb5c2975c2066ee7b",
+    "url": "/calendar/static/js/runtime-main.914f0415.js"
+  }
+]);
